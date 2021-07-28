7e81fe56e5d0522774e626de806ff1e5da9a3171
Douglas Young <dyoung4747@hotmail.com>
2021-07-28 20:01:53 +0000

    implement letterPositions

---
+++ letterPositions.js
@@ -0,0 +1,50 @@
+const eqArrays = function(arr1, arr2) {
+  let isTrue = "";
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      isTrue = false;
+    } else {
+      isTrue = true;
+    }
+  }
+  return isTrue;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  let isTrue = true;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      isTrue = false;
+    }
+  }
+  if (isTrue === true) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+
+
+const letterPositions = function(string) {
+  let result = {};
+  for (const index in string) {
+    if (string[index] !== " ") {
+      const letter = string[index];
+      if (result[letter]) {
+        result[letter].push(Number(index));
+      } else {
+        result[letter] = [Number(index)];
+      }
+    }
+  }
+  return result;
+};
+
+
+
+let string = "hello";
+console.log(letterPositions(string));
+
+assertArraysEqual(letterPositions(string).h, [0]);
+assertArraysEqual(letterPositions(string).l, [2, 3]);
