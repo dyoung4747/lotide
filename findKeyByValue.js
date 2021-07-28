fdad450e7295617512e071cbb28a4d9aff7d00b3
Douglas Young <dyoung4747@hotmail.com>
2021-07-28 21:04:16 +0000

    find key by value function

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, value) {
+  const keys = Object.entries(obj);
+  for (let i = 0; i < keys.length; i++) {
+    let val = keys[i][1];
+    let key = keys[i][0];
+    if (value === val) {
+      return key;
+    }
+  }
+};
+
+
+const musicByGenre = {
+  rap: "Drake",
+  rock: "Emery",
+  idm: "Rival Consoles"
+};
+
+console.log(findKeyByValue(musicByGenre, "Drake"));
+
+assertEqual(findKeyByValue(musicByGenre, "Drake"), "rap");
+assertEqual(findKeyByValue(musicByGenre, "Emery"), "rock");
+assertEqual(findKeyByValue(musicByGenre, "Rival Consoles"), "idm");
