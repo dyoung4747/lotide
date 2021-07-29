7d5f2317154dabe7f59b31648b564c3a5ffd4a49
Douglas Young <dyoung4747@hotmail.com>
2021-07-29 19:19:02 +0000

    added length check at beginning

--- eqArrays.js
+++ eqArrays.js
@@ -7,18 +7,20 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function (arr1, arr2) {
-  isTrue = "";
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  isTrue = true;
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       isTrue = false;
-    } else {
-      isTrue = true;
     }
   }
   return isTrue;
 };
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]))
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]))
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]))
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]))
 
 assertEqual(eqArrays([1, 2, 0], [1, 2, 0]), true);
+console.log(eqArrays([4, "2", "3"], ["1", "2", "3"]))
