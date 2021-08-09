fe3de62b7b7590c07487b1c7140dde92bf5e4388
Douglas Young <dyoung4747@hotmail.com>
2021-08-09 21:08:06 +0000

    refactored assertEqual/head

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,9 @@
 const assertEqual = function(actual, expected) {
   if (actual !== expected) {
-    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   } else if (actual === expected) {
-    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   }
 };
 
-assertEqual("Oh hi", "Oh hi");
-assertEqual("what", "where");
-assertEqual(289, 289);
-assertEqual(817, 22);
+module.exports = assertEqual;
