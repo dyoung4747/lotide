f05296e5765a9870c36263dd8a86c46b902e5411
Douglas Young <dyoung4747@hotmail.com>
2021-08-09 22:51:43 +0000

    function selects middle characters in array

--- middle.js
+++ middle.js
@@ -1,29 +1,3 @@
-const assertArraysEqual = function(arr1, arr2) {
-  let isTrue = true;
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      isTrue = false;
-    }
-  }
-  if (isTrue === true) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
-const eqArrays = function(arr1, arr2) {
-  let isTrue = "";
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      isTrue = false;
-    } else {
-      isTrue = true;
-    }
-  }
-  return isTrue;
-};
-
 const middle = function(array) {
   let midArr = [];
   for (let i = 0; i < array.length; i++) {
@@ -37,15 +11,5 @@ const middle = function(array) {
   }
   return midArr;
 };
-  
-  
 
-
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+module.exports = middle;
