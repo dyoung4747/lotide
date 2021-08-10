e26ba0149dfed17f1788dee23b74e2fdfdc24862
Douglas Young <dyoung4747@hotmail.com>
2021-08-10 20:35:02 +0000

    mocha and chai implementation

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,22 @@
-const assertEqual = require('../assertEqual');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-tail(["Guy", "Man", "Bro", "Man", "Dude"]);
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  it("returns ['Man', 'Bro', 'Man', 'Dude'] for ['Guy', 'Man', 'Bro', 'Man', 'Dude']", () => {
+    assert.deepEqual(tail(['Guy', 'Man', 'Bro', 'Man', 'Dude']), ['Man', 'Bro', 'Man', 'Dude']);
+  }),
 
-assertArraysEqual(tail(words), ["Lighthouse", "Labs"]);
+  it("returns [] for [1]", () => {
+    assert.deepEqual(tail([1]), []);
+  }),
+
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []);
+  })
+});
+
+// tail(["Guy", "Man", "Bro", "Man", "Dude"]);
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+// assertArraysEqual(tail(words), ["Lighthouse", "Labs"]);
